42b84c2d349da5db584a6789e721302a37c5a006
Dracudio <hanseddi90@gmail.com>
2025-12-05 14:39:54 +0100

    05-12-2025 update mit Aufgaben

--- Programmier_Projekt/main.c
+++ Programmier_Projekt/main.c
@@ -4,9 +4,13 @@ Spielprinzip von Studimon 3000:
 
 Menü:
 - Spiel starten
-- Einstellungen
-	* Art der Studimons für jeden Spieler (Analysis, Programmier, Schlüsselquali)
-	* Anzahl der Studimons pro Spieler
+	* Kampfausgabe
+	* Kampfaktionen + Studimon wechseln
+	* Studimon wechseln falls KO durch Kampfaktion
+	* Spieler wechseln
+- Spielkonfigurationen
+	* Art der Studimons für jeden Spieler auswählen
+	* Anzahl der Studimons pro Spieler festlegen
 - Beenden
 
 Studimon:
@@ -20,31 +24,41 @@ Studimon:
 */
 
 #include <stdio.h>
+#include <stdlib.h>   // rand, srand
+#include <time.h>     // time
 
-struct Studimon {
+typedef struct Aktion {
+	char name[30];
+	int staerke;
+	int Aktionstyp; // 0 = angriff, 1 = sp.angriff, 2 = Buff, 3 = Debuff
+	int genauigkeit;
+} Aktion;
+
+typedef struct Studimon {
+	char name[30];
 	int hp;
-	int mp;
-	int schaden;
 	int verteidigung;
+	int spverteidigung;
 	int agilitaet;
-	float kritschance;
-};
+	int kritschance;
+} Studimon;
 
 void zeigeMenue() {
 
 	printf("Willkommen in Studimon 3000\n");
 	printf("****************************\n");
 	printf("[0] Spiel starten\n");
-	printf("[1] Menue\n");
+	printf("[1] Spielkonfigurationen\n");
 	printf("[2] Beenden\n");
 	printf("-> ");
 }
 
 int main() {
 
+	srand(time(NULL));   // Seed setzen (SEHR wichtig!)
 	int AnzahlStudimons = 3;
 
-	int auswahl;
+	int auswahl;				// Variable für Auswahloptionen
 
 	do {
 		zeigeMenue();
@@ -54,9 +68,24 @@ int main() {
 
 		switch (auswahl) {
 		case 0:
+			// Das Spiel
 			printf("Spiel wird gestartet...\n");
+			// Programmieraufgabe:
+			// Zu entwickeln ist eine Funktion "angriff", die den Angriff eines Studimons auf ein anderes dastellt.
+			//
+			// Dabei sollen die HP des anzugreifenden Studimons per Referenz übergeben werden.
+			// Weitere benötigte Werte sollen als Parameter per Wert übergeben werden.
+			// Der Schaden wird berechnet aus: Stärke - Verteidigung
+			// 
+			// Das Studimon soll die Chance haben in der Funktion einen kritischen Treffer zu landen.
+			// Die Variabel Kirtchance ist ein integer und gibt die Wahrscheinlichkeit in Prozent an (z.B. 20 = 20%).
+			// Mit dem Code "rand() % 100 + 1" kann eine Zufallszahl zwischen 0 und 99 generiert werden.
+			// Falls das Studimon einen kritischen Treffer landet, soll der Schaden verdoppelt werden.
+			// 
+			// Es soll ebenfalls darauf geachtet werden, dass die HP nicht unter 0 fallen können und es keinen negativen Schaden gibt.
 			break;
 		case 1:
+			// Spielkonfigurationen
 			printf("Was möchtest du ändern?\n");
 			printf("[0] Studimons im Team ändern\n");
 			printf("[1] Anzahl der Studimons ändern\n");
@@ -66,6 +95,10 @@ int main() {
 			switch (auswahl) {
 			case 0:
 				printf("Studimons im Team ändern...\n");
+				// Programmieraufgabe:
+				// Zu entwickeln ist eine Funktion Erstelle_<StudimonName> die ein vollständiges Studimon erstellt, initialisiert und zurückgibt.
+				// Hier könnte ihr krativ sein und euch eigene Studimons ausdenken sowie ihre Werte und Aktionen.
+				// Es muss mindestens eine Aktion geben, die das Studimon im Kampf einsetzen kann.
 				break;
 			case 1:
 				do {
